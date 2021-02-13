b7b4bffd49c60bb28099cc6acea1dc17e5cb7a0e
Tait Haslam <tait.haslam@gmail.com>
2021-02-13 14:13:29 -0500

    completed CRUD methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,33 @@ class TasksController < ApplicationController
   def new
     @tasks = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def destory
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_tasks
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destory'
 end
